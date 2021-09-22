02233d45344a5778c6c73bb4e644c36c895ac036
kipnisa1 <abraham.kipnis@aalto.fi>
2021-09-22 11:34:37 +0300

    Adding fit_lattice.py test code

---
+++ fit_lattice.py
@@ -0,0 +1,11 @@
+from find_atom_positions import CircCorralData
+c = CircCorralData("Createc2_210729.150951.dat","Createc2_210729.150951.dat")
+c.subtract_plane()
+c.get_region_centroids(diamond_size=5, sigmaclip=2)
+# the box size to fit atom positions
+box_size_nm = 1.5
+box_size_pix = int(c.nm_to_pix(box_size_nm))
+c.fit_atom_pos_gauss(box_size=box_size_pix)
+c.corral = False
+c.occupied = False
+c.fit_lattice()
