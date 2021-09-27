f309a61c8c493dd2c33dc55c0f65565467e90c6c
kipnisa1 <abraham.kipnis@aalto.fi>
2021-09-27 11:35:39 +0300

    Make grid axes in nm rather than pixels

--- animate_grid.py
+++ animate_grid.py
@@ -4,7 +4,7 @@ import numpy as np
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
-import pdb
+from pdb import set_trace
 import scipy.signal
 import plotly.offline
 # %pylab inline
@@ -13,8 +13,10 @@ import pandas as pd
 import gif
 import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
+import createc
+import pdb
 
-def animate_cube(cube_array, cut=True, mn=0, sd=0, interval=75, cmap='hot', ptx=0, pty=0, ptx2=0, pty2=0):
+def animate_cube(cube_array, filename, cut=True, mn=0, sd=0, interval=75, cmap='hot', ptx=0, pty=0, ptx2=0, pty2=0):
     '''
     animates a python cube for quick visualisation.
 
@@ -38,6 +40,16 @@ def animate_cube(cube_array, cut=True, mn=0, sd=0, interval=75, cmap='hot', ptx=
     #for the 2.5nm radius empty corral these are at the middle / node
     # ptx, pty = 40, 40 # plot the LDOS from this pixel on the right
     # ptx2, pty2 = 30, 30
+    nmx, nmy = get_im_size(filename)
+    _, xpix, ypix = cube_array.shape
+
+    def pix_to_nm(pix):
+        assert xpix==ypix and nmx==nmy
+        return pix * nmx/xpix
+
+    def nm_to_pix(nm):
+        assert xpix==ypix and nmx==nmy
+        return nm*xpix/nmx*10
 
     cut = 3 # cut these points out from teh beginning and end of the pixel array
 
@@ -46,17 +58,23 @@ def animate_cube(cube_array, cut=True, mn=0, sd=0, interval=75, cmap='hot', ptx=
     # ax2 = plt.subplot2grid((1,3), (0, 2), colspan = 2)
     mn = np.mean(cube_array[-1][cut:-cut,cut:-cut])
     sd = np.std(cube_array[-1][cut:-cut,cut:-cut])
-    img = ax1.imshow(cube_array[-1][cut:-cut,cut:-cut], animated=True,  cmap=cmap, vmax=mn+3*sd, vmin=mn-3*sd)
-
+    img = ax1.imshow(cube_array[-1][cut:-cut,cut:-cut],
+                    animated=True,
+                    cmap=cmap,
+                    vmax=mn+3*sd,
+                    vmin=mn-3*sd,
+                    extent=[0, nmx/10.,0, nmy/10.])
+    ax1.set_xlabel("nm")
+    ax1.set_ylabel("nm")
     # s1 and s2 are PathCollection objects
     # can use s1.get_sizes() to look at the default size
     s1 = ax1.scatter([ptx],[pty], s=20)
     s2 = ax1.scatter([ptx2],[pty2], s=20) #default size is 36
-    # pdb.set_trace()
     cbar = fig.colorbar(img, ax=ax1, shrink=0.6)
 
-    title = ax1.text(0.5,0.85, 'V= %1.2lf mV' %(specvz3[0,0]), bbox={'facecolor':'w', 'alpha':0.5, 'pad':5},
-                 transform=ax1.transAxes, ha="center")
+    title = ax1.text(0.5,0.85, 'V= %1.2lf mV' %(specvz3[0,0]),
+                bbox={'facecolor':'w', 'alpha':0.5, 'pad':5},
+                transform=ax1.transAxes, ha="center")
 
     # cut off some points from end of spectra because dIdV saturates at the end
     # probably due to not setting the bias to the starting point of the spectra
@@ -65,8 +83,10 @@ def animate_cube(cube_array, cut=True, mn=0, sd=0, interval=75, cmap='hot', ptx=
     # we have to either manually change the axes limits or just cut off part of the data in the visualisationquit()
 
     offset = 10
-    ax2.plot(specvz3[:,-1][offset:-1],cube_array[:,ptx, pty][offset:-1])
-    ax2.plot(specvz3[:,-1][offset:-1],cube_array[:,ptx2, pty2][offset:-1])
+
+    px, py, px2, py2 = map(int,map(nm_to_pix,[ptx, pty, ptx2, pty2]))
+    ax2.plot(specvz3[:,-1][offset:-1],cube_array[:,px, py][offset:-1])
+    ax2.plot(specvz3[:,-1][offset:-1],cube_array[:,px2, py2][offset:-1])
 
     ax2.yaxis.tick_right()
     ax2.set_xlabel("Bias (mV)")
@@ -93,8 +113,8 @@ def animate_cube(cube_array, cut=True, mn=0, sd=0, interval=75, cmap='hot', ptx=
         # cbar.draw_all()
         # ax2.remove()
         ax2.clear()
-        ax2.plot(specvz3[:,0][offset:-1],cube_array[:,ptx, pty][offset:-1])
-        ax2.plot(specvz3[:,0][offset:-1],cube_array[:,ptx2, pty2][offset:-1])
+        ax2.plot(specvz3[:,0][offset:-1],cube_array[:,px, py][offset:-1])
+        ax2.plot(specvz3[:,0][offset:-1],cube_array[:,px2, py2][offset:-1])
 
         ax2.axvline(specvz3[i,0], c='r')
         title2 = ax2.text(0.5,1.1, "dI/dV point spectra from grid", #bbox={'facecolor':'w', 'alpha':1, 'pad':5}
@@ -110,10 +130,20 @@ def animate_cube(cube_array, cut=True, mn=0, sd=0, interval=75, cmap='hot', ptx=
     # plt.show()
     ani.save(filename+'_cube_movie.mp4', writer="ffmpeg", fps=28)
 
+
+def get_im_size(gridfile):
+    f = open(gridfile+".dat","rb")
+    d = f.readlines()
+    a = [str(b).split('\\t') for b in d]
+    ls = [float(b.split("=")[-1].rstrip(" \\r\\n'")) for b in np.array(a[0:600]).T[0] if "Length" in b]
+    return ls #in angstroms
+
+
 if __name__ == "__main__":
-    # read size of image from /Users/akipnis/Desktop/Aalto Atomic Scale Physics/Summer 2021 Corrals Exp data/Ag 2021-08-16 2p5 nm radius empty/Createc2_210816.223358.specgrid.dat
+    # read size of image from .specgrid.dat file
     dir = "/Users/akipnis/Desktop/Aalto Atomic Scale Physics/Summer 2021 Corrals Exp data/"
     filename= dir +r"Ag 2021-08-13 2p5 nm radius/grid/Createc2_210814.214635.specgrid"
+    # datfile = filename
     # filename = dir +r'Ag 2021-08-16 2p5 nm radius empty/Createc2_210816.223358.specgrid'
     # filename = dir+r'/Ag 2021-08-12 4p5 nm radius/grid/Createc2_210813.001749.specgrid'
 
@@ -163,13 +193,10 @@ if __name__ == "__main__":
     data = np.fromfile(f, dtype=np.float32)
     f.close
 
-    # pdb.set_trace()
-
     a, b = int(nx/specgriddx), int(ny/specgriddy)
     try:
         specdata = data.reshape(a,b,len(specvz3),int(len(data)/a/b/len(specvz3)))
     except:
         a, b = xend, yend
         specdata = data.reshape(a,b,len(specvz3),int(len(data)/a/b/len(specvz3)))
-    # pdb.set_trace()
-    animate_cube(specdata[:,:,:,1].T, False, ptx= 45, pty=44, ptx2=52, pty2=44)
+    animate_cube(specdata[:,:,:,1].T, filename, cut=False, ptx=3.3, pty=3.6, ptx2=3.3, pty2=4)
