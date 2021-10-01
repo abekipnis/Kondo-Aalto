9ac2346713000bccb59b61357587afc342582504
Kipnis Abraham <kipnisa1@login3.triton.aalto.fi>
2021-10-01 17:51:29 +0300

    Running fit_lattice on Triton

--- fit_lattice.py
+++ fit_lattice.py
@@ -3,6 +3,7 @@ from numpy import array
 import pdb
 import numpy as np
 import scattering_model
+import matplotlib.pyplot as plt
 #c = CircCorralData("test/Createc2_210811.092547.dat","test/Createc2_210811.092547.dat")
 c = CircCorralData("test/Createc2_210813.102220.dat","Createc2_210813.102220")
 
@@ -29,13 +30,17 @@ c.r_g, c.c_g = c.nsphere_fit(atoms_g)
 
 c.compare_fits()
 atompoints, angle, offseta, offsetb, latt = c.fit_lattice(niter=20)
-erange = np.arange(-0.020, 0.020, 0.001)
+erange = np.arange(-0.020, 0.020, 0.005)
 
 # this takes way too long if using the generated lattice from the fit
 # better to use lattice generated from numpy mesh
 #spectra = scattering_model.gs(atompoints, latt, erange, c.c_g)
 #plt.plot(erange, spectra); plt.imshow()
-
-spectrum = scattering_model.get_spectra(atompoints/100, 30, 4, erange) #radius doesn't matter much here, just has to be larger than the radius of the actual atoms (can hard code this, given we have c.r)
 pdb.set_trace()
-plt.plot(erange, spectra); plt.savefig("spectrum_test.png")
+#get_spectra(atom_locs (in nm), n_sites (i.e. box size in pixels), r (radius in nm), erange)
+spectrum = scattering_model.get_spectra(atompoints/100, 10, 4, erange) #radius doesn't matter much here, just has to be larger than the radius of the actual atoms (can hard code this, given we have c.r)
+spectrum = np.array(spectrum)
+for i, e in enumerate(erange):
+	plt.close(); plt.imshow(spectrum[i,:,:]); plt.savefig("spectrum_testi_%1.2lf.png" %(e))
+plt.close(); plt.plot(erange, spectrum[:,5,5]); plt.savefig("spectrum_test.png")
+
