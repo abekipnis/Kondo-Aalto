f254f688fca90c2768bb6e2587924daf6ad542d7
Kipnis Abraham <kipnisa1@login3.triton.aalto.fi>
2021-10-01 18:10:31 +0300

    fixing conflicts

--- fit_lattice.sh
+++ fit_lattice.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #SBATCH --time=04:00:00      # 4 hours
 #SBATCH --mem=1000M   # 1G of memory
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=2G
+#SBATCH -o model_LDOS.out
 
+export OMP_PROC_BIND=true
+echo 'Running on :'$HOSTNAME
 srun python3 fit_lattice.py
