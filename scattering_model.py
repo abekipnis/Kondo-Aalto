a7571292ee10cfaf25e48e2500a31b137ac03e2b
kipnisa1 <abraham.kipnis@aalto.fi>
2021-10-04 17:31:01 +0300

    Scattering model

--- scattering_model.py
+++ scattering_model.py
@@ -2,15 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import distance_matrix
 import pint
+from pint import set_application_registry
 import cmath
 import numpy as np
 import multiprocessing
-from multiprocessing import Pool
 import pdb
 from time import time
 from multiprocessing import Pool, freeze_support
-import matplotlib.pyplot as plt
+from multiprocessing.pool import ThreadPool
+
+
+import multiprocessing.pool
+
+
 ureg = pint.UnitRegistry()
+set_application_registry(ureg)
 Q_ = ureg.Quantity
 
 hbar = 1.0545718e-34 * ureg.joule * ureg.second
@@ -33,17 +39,22 @@ def k(E, m_e, E0):
     K = (2*m_e*(E-E0)/(hbar**2))**.5#a
     return K
 
-def LDOS_at_point(x, y, A, k_tip, atom_locs):
-    n_atoms = len(atom_locs)
-    aT = [at(Q_(np.linalg.norm([x,y]-atomloc.magnitude),"nm"),k_tip.to("1/nm")) for atomloc in atom_locs]
-    a0 = [a(Q_(np.linalg.norm([x,y]-atomloc.magnitude),"nm"),k_tip.to("1/nm"),1,1) for atomloc in atom_locs]
-    ld = 2*np.dot(np.dot(np.array(aT),np.linalg.inv(np.ones(n_atoms) -A)),a0).real
+def LDOS_at_point(x, y, A, k_tip, atom_locs, n_atoms):
+    delta0 = np.pi/4
+    alpha0 = 0
+    aT = [at(Q_(np.linalg.norm([x,y]-atomloc.magnitude),"nm"), k_tip.to("1/nm")) for atomloc in atom_locs]
+    a0 = [a(Q_(np.linalg.norm([x,y]-atomloc.magnitude),"nm"), k_tip.to("1/nm"),delta0, alpha0) for atomloc in atom_locs]
+    #
+    # aT = [at(np.linalg.norm([x,y]-atomloc),k_tip) for atomloc in atom_locs]
+    # a0 = [a(np.linalg.norm([x,y]-atomloc),k_tip,delta0,alpha0) for atomloc in atom_locs]
+    ones = np.ones(n_atoms)
+    ld = 2*np.dot(np.dot(np.array(aT),np.linalg.inv(ones-A)),a0).real
     return ld
 
 def get_atom_locs(n_atoms, radius): #radius in nm
-    atom_locs = Q_(np.asarray([[radius * np.cos(n*2*np.pi/n_atoms),
+    atom_locs = np.asarray([[radius * np.cos(n*2*np.pi/n_atoms),
                         radius * np.sin(n*2*np.pi/n_atoms)]
-                        for n in range(1,n_atoms+1)]), 'nm')
+                        for n in range(1,n_atoms+1)])
     return atom_locs
 
 def create_A_matrix(n_atoms, atom_locs, k_tip):
@@ -56,16 +67,13 @@ def create_A_matrix(n_atoms, atom_locs, k_tip):
                 A[n][m] = 1
             else:
                 A[n][m] = a(Q_(
-                    np.linalg.norm(atom_locs[n].magnitude-atom_locs[m].magnitude)
-                                ,"nm"),
+                    np.linalg.norm(atom_locs[n].magnitude-atom_locs[m].magnitude),"nm"),
                     k_tip.to("1/nm"),
                     delta0, # d0
                     alpha0) # a0
     return A
 
-def calc_LDOS(atom_locs, nmxyrange, k_tip):
-    n_atoms = len(atom_locs)
-    atom_locs -= np.mean(atom_locs, axis=0)
+def calc_LDOS(atom_locs, nmxyrange, k_tip, n_atoms):
     a0 = np.zeros(n_atoms)
     aT = np.zeros(n_atoms)
     m = Q_(np.asarray(nmxyrange),"nm")
@@ -74,24 +82,24 @@ def calc_LDOS(atom_locs, nmxyrange, k_tip):
     X, Y = np.meshgrid(m, m)
     LDOS = np.zeros((n_sites,n_sites))
     A = create_A_matrix(n_atoms, atom_locs, k_tip)
-    for n in range(n_sites):
-        for m in range(n_sites):
-            LDOS[n][m] = LDOS_at_point(X[n][m].magnitude, Y[n][m].magnitude,A, k_tip, atom_locs)
-    return LDOS
 
+    p = np.array([[(X[n][m].magnitude, Y[n][m].magnitude, A, k_tip , atom_locs,n_atoms) for n in range(n_sites)] for m in range(n_sites)]).reshape(n_sites*n_sites,6)
+    with Pool(5) as pool:
+        LDOS = pool.starmap(LDOS_at_point,p)
+
+    return LDOS
 
 def c_LDOS(atom_locs, latt_sites, k_tip):
     n_atoms = len(atom_locs)
     a0 = np.zeros(n_atoms)
     aT = np.zeros(n_atoms)
-    m = Q_(latt_sites,"nm")
+    m = latt_sites #nm
     LDOS = np.zeros(len(latt_sites))
     A = create_A_matrix(n_atoms, atom_locs, k_tip)
 
     for n0, n in enumerate(latt_sites):
         LDOS[n0] = LDOS_at_point(n[0], n[1], A, k_tip, atom_locs)
 
-    pdb.set_trace()
     plt.scatter(*np.array(latt_sites).T, c=LDOS)
     plt.colorbar()
     plt.show()
@@ -102,7 +110,7 @@ def gs(atom_locs, latt_sites, erange, spectrumpt):
     # where in the lattice array do we need to index into to get the
     # spectrum at the point we want?
     speclatidx = np.argmin([np.linalg.norm(spectrumpt-l) for l in latt_sites])
-    atom_locs = Q_(atom_locs, "nm")
+    atom_locs = atom_locs
     for e in erange:
         E = Q_(e,"volt")*electron_charge
         k_tip = k(E, m_e, E_0)
@@ -110,21 +118,21 @@ def gs(atom_locs, latt_sites, erange, spectrumpt):
         s.append(LDOS[speclatidx])
     return s
 
-def get_LDOS(e, atom_locs, nmxyrange):
+def get_LDOS(e, atom_locs, nmxyrange,n_atoms):
     ts = time()
     print(multiprocessing.current_process())
     E = Q_(e,"volt")*electron_charge
     k_tip = k(E, m_e, E_0)
-    LDOS = calc_LDOS(atom_locs, nmxyrange, k_tip)
-    print(time()-ts)
-    return LDOS
+    LDOS = calc_LDOS(atom_locs, nmxyrange, k_tip,n_atoms)
+    print("time to get LDOS map:", time()-ts)
+    return np.array(LDOS).reshape(len(nmxyrange),len(nmxyrange))
 
 def get_spectra(atom_locs, nmxyrange, erange):
-    atom_locs = Q_(atom_locs, "nm")
     s = []
+    atom_locs = Q_(atom_locs, "nm")
     print(multiprocessing.cpu_count())
-
-    p = [(e, atom_locs, nmxyrange) for e in erange]
-    with Pool(5) as pool:
+    n_atoms = len(atom_locs)
+    p = [(e, atom_locs, nmxyrange,n_atoms) for e in erange]
+    with ThreadPool(5) as pool:
         s = pool.starmap(get_LDOS, p)
     return s
