e42df728f7e61d769f9a0cdd002bb61600b5d21c
Kipnis Abraham <kipnisa1@login3.triton.aalto.fi>
2022-02-08 22:11:36 +0200

    commit from triton

--- test_scattering_model.sh
+++ test_scattering_model.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #SBATCH --time=04:00:00      # 4 hours
-#SBATCH --mem=1000M   # 1G of memory
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=1G
+#SBATCH --mem=2G
 #SBATCH -o model_LDOS.out
 
 export OMP_PROC_BIND=true
 echo 'Running on :'$HOSTNAME
 #srun python3 fit_lattice.py --emin=-0.02 --emax=0.02 --n_es=5 --ngridpoints=100
+
+
+# run this with sbatch test_scattering_model.sh 
+
+
 srun python3 test_scattering_model.py --emin=-0.066 --emax=0.3 --n_es=10 --ngridpoints=100 --path="test/Createc2_210812.170231.dat"
---
+++ test_scattering_model_run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 test_scattering_model.py --emin=-0.066 --emax=0.3 --n_es=5 --ngridpoints=100 --path="test/Createc2_210813.102220.dat"
