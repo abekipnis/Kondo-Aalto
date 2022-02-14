fe8993a482dad2b433e44e2cc484e0b0152e3988
kipnisa1 <abraham.kipnis@aalto.fi>
2022-02-14 21:28:02 +0200

    Test scattering phase shift

--- scattering/test_scattering_model.sh
+++ scattering/test_scattering_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=04:00:00      # 4 hours
+#SBATCH --time=08:00:00      # 4 hours
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=2G
+#SBATCH --mem=14G
 #SBATCH -o model_LDOS.out
 
 export OMP_PROC_BIND=true
@@ -9,7 +9,7 @@ echo 'Running on :'$HOSTNAME
 #srun python3 fit_lattice.py --emin=-0.02 --emax=0.02 --n_es=5 --ngridpoints=100
 
 
-# run this with sbatch test_scattering_model.sh 
+# run this with sbatch test_scattering_model.sh
 
-
-srun python3 test_scattering_model.py --emin=-0.066 --emax=0.3 --n_es=10 --ngridpoints=100 --path="test/Createc2_210812.170231.dat"
+srun python3 test_scattering_model.py
+# srun python3 test_scattering_model.py --emin=-0.066 --emax=0.3 --n_es=10 --ngridpoints=100 --path="test/Createc2_210812.170231.dat"
