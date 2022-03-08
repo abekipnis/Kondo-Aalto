4f12bd800c4510695d730d6c3494419829af5ab0
kipnisa1 <abraham.kipnis@aalto.fi>
2022-03-08 11:52:36 +0200

    Added requirements.txt

--- test_scattering_model.py
+++ test_scattering_model.py
@@ -10,7 +10,7 @@ from numpy import array
 import pdb
 import numpy as np
 from AaltoAtoms import scattering_simulation as ss
-
+from AaltoAtoms import args
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import argparse
@@ -33,28 +33,12 @@ python3 test_scattering_model.py --emin=-0.02 --emax=0.02 --n_es=5 --ngridpoints
 # TODO: compare CH maps with unoccupied corrals
 # TODO: compare with line spectrum from empty corrals
 
-def get_args(parser):
-	parser.add_argument("--emin", type=float, default=-0.05)
-	parser.add_argument("--emax", type=float, default=0.1)
-	parser.add_argument("--n_es", type=int, default=20)
-	parser.add_argument("--ngridpoints", type=int, default=20)
-
-	# "/m/phys/project/asp/labdata/Createc_new/STMDATA/Ag/Small Kondo corrals/"
-	d_dat = "test/Createc2_210816.170832.dat"
-	d_linespec_dir = "Ag 2021-08-16 2p5 nm radius empty/3p8 nm pm100mV line/"
-
-	parser.add_argument("--path", type=str, default=d_dat)
-	parser.add_argument("--linespec_dir", type=str, default=d_linespec_dir)
-
-	args = parser.parse_args()
-	return args
-
 if __name__=="__main__":
 	host = socket.gethostname()
 	print("Running on host: ", host)
 
 	warnings.filterwarnings("ignore")
-	args = get_args(argparse.ArgumentParser())
+	args = args.get_args(argparse.ArgumentParser())
 	path = args.path								# the path to the .dat file
 	linespec_dir = args.linespec_dir				# path to folder with spectra
 	emin = args.emin								# min of energy range for model
@@ -81,59 +65,59 @@ if __name__=="__main__":
 
 	t = time()
 
-	erange = np.arange(emin, emax, (emax-emin)/n_es)
-	print("Getting spectra for erange: ", erange)
-	nmxyrange = c.pix_to_nm(np.arange(0,c.xPix, c.xPix/ngridpoints))
-	l = sm.spectrum_along_line(c.pix_to_nm(atoms_g), erange)
-	np.save("%s_line_spectrum.npy" %(fname_head), l)
-	plt.imshow(np.flipud(np.array(l).T), extent=(0, 10, min(erange), max(erange)), aspect="auto")
-	plt.show()
-	pdb.set_trace()
-
-	args = [c.pix_to_nm(atoms_g), nmxyrange, erange]
-	spectrum = np.array(sm.get_spectra(*args))
-
-	for i, e in enumerate(erange):
-		plt.close();
-		spectrum_data = spectrum[i,:,:]
-		np.save("%s_spectrum_%1.2lf.npy" %(fname_head,e), spectrum_data)
-		extent = [0,c.pix_to_nm(c.xPix),0,c.pix_to_nm(c.xPix)]
-		plt.imshow(np.rot90(spectrum_data.T), extent=extent);
-		x,y = np.array(list(map(c.pix_to_nm,c.gauss_fit_locs)))
-		plt.scatter(x,y)
-		plt.colorbar()
-		plt.savefig("%s_spectrum_%1.2lf.png" %(fname_head,e))
-	plt.close();
-
-	fig, (ax1) = plt.subplots(1, 1)
-	img = ax1.imshow(np.flipud(np.rot90(spectrum[0,:,:])),
-					animated=True,
-					extent=[0,c.pix_to_nm(c.xPix),0,c.pix_to_nm(c.xPix)])
-	ax1.set_xlabel("nm")
-	ax1.set_ylabel("nm")
-
-	ax1.set_title("LDOS(V,r)")
-
-	plt.suptitle("Test scattering model", y=0.95)
-	def updatefig(i):
-		d = spectrum[i,:,:]
-
-		img.set_array(np.flipud(np.rot90(d)))
-
-		return img,
-	anim = animation.FuncAnimation(fig, updatefig, frames=len(erange), interval=50, blit=True) #interval in ms
-	try:
-		try:
-			anim.save('%s_cube_movie.mp4' %(fname_head), writer="ffmpeg", fps=28)
-		except:
-			anim.save('%s_cube_movie.mp4' %(fname_head), fps=28)
-	except:
-		print("could not save animation :(")
-	plt.show()
-
-	plt.plot(erange, spectrum[:,5,5]);
-	plt.savefig("spectrum_test.png")
-
+	# erange = np.arange(emin, emax, (emax-emin)/n_es)
+	# print("Getting spectra for erange: ", erange)
+	# nmxyrange = c.pix_to_nm(np.arange(0,c.xPix, c.xPix/ngridpoints))
+	# l = sm.spectrum_along_line(c.pix_to_nm(atoms_g), erange)
+	# np.save("%s_line_spectrum.npy" %(fname_head), l)
+	# plt.imshow(np.flipud(np.array(l).T), extent=(0, 10, min(erange), max(erange)), aspect="auto")
+	# plt.show()
+	# pdb.set_trace()
+	#
+	# args = [c.pix_to_nm(atoms_g), nmxyrange, erange]
+	# spectrum = np.array(sm.get_spectra(*args))
+	#
+	# for i, e in enumerate(erange):
+	# 	plt.close();
+	# 	spectrum_data = spectrum[i,:,:]
+	# 	np.save("%s_spectrum_%1.2lf.npy" %(fname_head,e), spectrum_data)
+	# 	extent = [0,c.pix_to_nm(c.xPix),0,c.pix_to_nm(c.xPix)]
+	# 	plt.imshow(np.rot90(spectrum_data.T), extent=extent);
+	# 	x,y = np.array(list(map(c.pix_to_nm,c.gauss_fit_locs)))
+	# 	plt.scatter(x,y)
+	# 	plt.colorbar()
+	# 	plt.savefig("%s_spectrum_%1.2lf.png" %(fname_head,e))
+	# plt.close();
+	#
+	# fig, (ax1) = plt.subplots(1, 1)
+	# img = ax1.imshow(np.flipud(np.rot90(spectrum[0,:,:])),
+	# 				animated=True,
+	# 				extent=[0,c.pix_to_nm(c.xPix),0,c.pix_to_nm(c.xPix)])
+	# ax1.set_xlabel("nm")
+	# ax1.set_ylabel("nm")
+	#
+	# ax1.set_title("LDOS(V,r)")
+	#
+	# plt.suptitle("Test scattering model", y=0.95)
+	# def updatefig(i):
+	# 	d = spectrum[i,:,:]
+	#
+	# 	img.set_array(np.flipud(np.rot90(d)))
+	#
+	# 	return img,
+	# anim = animation.FuncAnimation(fig, updatefig, frames=len(erange), interval=50, blit=True) #interval in ms
+	# try:
+	# 	try:
+	# 		anim.save('%s_cube_movie.mp4' %(fname_head), writer="ffmpeg", fps=28)
+	# 	except:
+	# 		anim.save('%s_cube_movie.mp4' %(fname_head), fps=28)
+	# except:
+	# 	print("could not save animation :(")
+	# plt.show()
+	#
+	# plt.plot(erange, spectrum[:,5,5]);
+	# plt.savefig("spectrum_test.png")
+	#
 
 	# pdb.set_trace()
 	# s = sm.get_spectrum_at_middle(c.pix_to_nm(atoms_g), erange)
