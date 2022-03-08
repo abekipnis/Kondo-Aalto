cb9a15f9eeb03c4d929bda377b956cfd9eb743fa
kipnisa1 <abraham.kipnis@aalto.fi>
2022-03-08 11:52:36 +0200

    Added requirements.txt

--- AaltoAtoms/__init__.py
+++ AaltoAtoms/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.0.0"
 from .Kondo_data_analysis import read_vertfile
 from .find_atom_positions import CircCorralData
-from .scattering import scattering_simulation
+from .scattering import scattering_simulation, args
 from .Kondo_data_analysis.read_vertfile import Spec, fit_data_fixed_vals, fit_data_w_times_residual
