701925b9cc0984ce5e507adc40d184c763eaae6d
ccakarolotw <ccakarolotw@gmail.com>
2022-04-11 14:32:30 +0300

    encapsulating clockwiseangle function

--- AaltoAtoms/find_atom_positions.py
+++ AaltoAtoms/find_atom_positions.py
@@ -724,6 +724,27 @@ class CircCorral(CircCorralData):
         self.bias = 0.0
         self.current = 0.0
 
+def clockwiseangle(point, origin, refvec):
+    # Vector between point and the origin: v = p - o
+    vector = [point[0]-origin[0], point[1]-origin[1]]
+    # Length of vector: ||v||
+    lenvector = math.hypot(vector[0], vector[1])
+    # If length is zero there is no angle
+    if lenvector == 0:
+        print("zero vector")
+        return 2*np.pi
+    # Normalize vector: v/||v||
+    normalized = [vector[0]/lenvector, vector[1]/lenvector]
+    dotprod  = normalized[0]*refvec[0] + normalized[1]*refvec[1]     # x1*x2 + y1*y2
+    diffprod = refvec[1]*normalized[0] - refvec[0]*normalized[1]     # x1*y2 - y1*x2
+    angle = math.atan2(diffprod, dotprod)
+    # Negative angles represent counter-clockwise angles so we need to subtract them
+    # from 2*pi (360 degrees)
+    #if angle < 0:
+     #   return 2*math.pi+angle
+    # I return first the angle because that's the primary sorting criterium
+    # but if two vectors have the same angle then the shorter distance should come first.
+    return angle
 
 def round_to_even(n):
     # return n rounded up to the nearest even integer
