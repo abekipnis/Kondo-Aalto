30e671057a03283176d9d7a61fa68bc4e60c119b
ccakarolotw <ccakarolotw@gmail.com>
2022-04-11 14:32:30 +0300

    encapsulating clockwiseangle function

--- AaltoAtoms/__init__.py
+++ AaltoAtoms/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.0.0"
 from .Kondo_data_analysis import read_vertfile
-from .find_atom_positions import CircCorralData, CircCorral
+from .find_atom_positions import CircCorralData, CircCorral, clockwiseangle
 from .scattering import scattering_simulation, args
 from .visualizations import show_line_spectrum
 from .Kondo_data_analysis.read_vertfile import Spec, fit_data_fixed_vals, fit_data_w_times_residual
