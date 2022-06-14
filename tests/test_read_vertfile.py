2aec6f8b914a5f1f74346de1fe0f3ce1bdfeb293
abekipnis <abekipnis@yahoo.com>
2022-06-14 16:03:29 +0300

    added numpy to test

--- tests/test_read_vertfile.py
+++ tests/test_read_vertfile.py
@@ -2,10 +2,11 @@ from AaltoAtoms import Spec
 
 class TestSpec():
     import numpy as np
+    test_file = r"tests\Createc2_210811.113827.L0001.VERT"
+
     def test_init(self):
-        file = r"tests\Createc2_210811.113827.L0001.VERT"
-        S = Spec(file)
-        assert(S.fname==file)
+        S = Spec(test_file)
+        assert(S.fname==test_file)
         assert(S.NPoints==512)
         assert(np.isclose(S.FBLogiset, 1500, 0.1))
         assert(np.isclose(S.biasVoltage, 100, 1))
